534611b8e5c9e7c1f1e9408eedbb4b8bb894cb6a
CJ <>
2002-07-11 23:40:30 +0100

    Demo Quest for Adventure Game Studio v2.4.0

---
+++ DemoQuest/Roomhdr.sh
@@ -0,0 +1,8 @@
+// Main header script - this will be included into every script in
+// the game (local and global). Do not place functions here; rather,
+// place import definitions and #define names here to be used by all
+// scripts.
+   import function face_up();
+   import function face_down();
+   import function face_left();
+   import function face_right();
